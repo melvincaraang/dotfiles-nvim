4789227e5c4bf752b3a445a8d4c3c6386cf61abe
Melvin Caraang <melvinj.caraang@gmail.com>
2025-11-08 12:13:00 -0800

    Fix treesitter config error

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -22,12 +22,25 @@ return {
     {
         "nvim-treesitter/nvim-treesitter-context",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
-        opts = {
-            enable = true,
-            max_lines = 0,
-            trim_scope = "outer",
-            mode = "cursor",
-            separator = nil,
-        },
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            require("treesitter-context").setup({
+                enable = true,
+                max_lines = 0,
+                trim_scope = "outer",
+                mode = "cursor",
+                separator = nil,
+                on_attach = function(bufnr)
+                    -- Validate that windows are valid before attaching
+                    local win_ids = vim.api.nvim_list_wins()
+                    for _, win in ipairs(win_ids) do
+                        if vim.api.nvim_win_get_buf(win) == bufnr then
+                            return true
+                        end
+                    end
+                    return false
+                end,
+            })
+        end,
     },
 }
